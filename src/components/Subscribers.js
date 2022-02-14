260f0c1344ded531a55983fc05d6976009e3fb7c
Anton Iudin <yudin431@gmail.com>
2022-02-14 13:32:51 -0500

    gitignore

---
+++ src/components/Subscribers.js
@@ -0,0 +1,8 @@
+import React from 'react';
+ 
+ export default function Subscribers(props) {
+    return <div className='subscribers'>
+        <div className="heartIcon"></div>
+        supporters
+    </div>;
+ }
