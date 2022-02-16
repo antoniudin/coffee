94e2a728e49a7477d97cf31edce35eeecd66444e
Anton Iudin <yudin431@gmail.com>
2022-02-15 19:19:04 -0500

    Photo block completed

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../navigation.css'
 import NavBar from '../components/NavBar'
+import Buttons from '../components/Buttons'
+import Portrait from '../components/Portrait'
 
-export default function FAQ() {
+export default function Explore(props) {
+  
+  const [toggle, setToggle] = useState('Artists')
+
+  const persons = [
+    {id:1, name:'Jane', text:'she does somethin on youtube', num: 256, link: 'photo'},
+    {id:2, name:'Christina', text:'nobody', num: 132, link: 'photo2'},
+    {id:3, name:'Olga', text:'lorem ipsum', num: 445, link: 'photo3'},
+    {id:4, name:'Natasha', text:'text text text', num: 227, link: 'photo4'}
+  ]
+
+  // const id = 1;
+
+  function func (event) {
+    setToggle(event.target.id)
+    
+  }
+
   return (
     <div className='page'>
         <NavBar/>
-        explore
+        {/* <p>{props.home}</p> */}
+
+        <Buttons action={func} active={toggle}/>
+        {toggle=='Artists' && <div className="horizontal">
+          {persons.map(person=> <Portrait key={person.id} name={person.name} text={person.text} num={person.num} class={person.link}/>)}
+        </div>}
+
+        {toggle=='Writers' && <div className="horizontal">
+          Writers are here
+        </div>}
+
+        {toggle=='Musicians' && <div className="horizontal">
+        Musicians are here
+        </div>}
+
+        {toggle=='Developers' && <div className="horizontal">
+        Developers are here
+        </div>}
     </div>
   )
 }
